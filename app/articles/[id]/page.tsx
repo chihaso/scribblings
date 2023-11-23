30777260d9d4fb5ab7f39609e489e9060d4eea04
chihaso <cycyccq@gmail.com>
2023-11-23 20:03:17 +0900

    RichText内に埋め込んだ画像を表示できるようにする

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -1,7 +1,9 @@
 import { contentfulClient } from '@/app/lib/contentfulClient'
 import { BlogEntrySkeleton } from '@/app/types/contentful'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { BLOCKS, Node as RichTextNode } from '@contentful/rich-text-types'
 import style from './styles.module.css'
+import Image from 'next/image'
 
 type Props = {
   params: { id: string }
@@ -17,13 +19,26 @@ export async function generateStaticParams() {
 
 export default async function Article({ params }: Props) {
   const entry = await contentfulClient.getEntry<BlogEntrySkeleton>(params.id)
-
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: (node: RichTextNode) => (
+        <Image
+          src={`https:${node.data.target.fields.file.url}`}
+          height={node.data.target.fields.file.details.image.height}
+          width={node.data.target.fields.file.details.image.width}
+          alt={node.data.target.fields.description}
+        />
+      ),
+    },
+  }
   return (
     <div className={`${style.article} mx-auto border rounded w-4/5 max-w-3xl p-5`}>
       <article>
         <h2 className="text-2xl">{entry.fields.title}</h2>
         <p className="date mt-3">{entry.sys.createdAt}</p>
-        <section className="mt-5 text-lg">{documentToReactComponents(entry.fields.body)}</section>
+        <section className="mt-5 text-lg">
+          {documentToReactComponents(entry.fields.body, options)}
+        </section>
       </article>
     </div>
   )
